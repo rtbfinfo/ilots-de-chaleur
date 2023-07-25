bc7ff32a45032f4f9b144e9da9e21a97fc703875
Héloïse Feldmann <81630954+Yheloww@users.noreply.github.com>
2023-07-25 15:49:06 +0200

    Revert "start d3 tests"
    This reverts commit f891b3038130f8e990c3d29201fb413a1ce3d59f.

--- ilots/src/routes/+page.svelte
+++ ilots/src/routes/+page.svelte
@@ -2,44 +2,35 @@
     import * as d3 from "d3"
     import Map from "./components/Map.svelte";
     import { onMount } from "svelte";
-    import { updated } from "$app/stores";
 
     onMount(async () => {
 
-   d3.json("https://gist.githubusercontent.com/Yheloww/b871de29011161706a3ae896690f65f7/raw/90e0574e594379bee3907f062048726526ff88b6/liege_sel_centroid.json")
-   .then(function(data) {
-    console.log(data)
-    update(data);
-   })
+        async function fetchLiege() {
+        const response = await fetch('https://raw.githubusercontent.com/rtbfinfo/ilots-de-chaleur/main/assets/selection/liege_sel_centroid.csv?token=GHSAT0AAAAAACFAIAPDK2RU3IH2CFY2RBY2ZF7TU2A');
+        console.log(response)
+        return await response.json()
+    }
+      
+    let geoJson = fetchLiege()
 
-    
+    let projection = d3.geoEquirectangular;
 
-    function update(data) {
-
-        let projection = d3.geoEquirectangular;
-
-        let geoGenerator = d3.geoPath()
+    let geoGenerator = d3.geoPath()
                        .projection(projection)
 
-        const svg = d3.select("#map")
-        .append("svg")
-        .attr("width", 500)
-        .attr("height", 500);
-
-        svg.append('g').selectAll('path')
-        .data(data.features)
-        .enter().append('path')
-        .attr('d', d3.geoPath().projection(d3.geoEquirectangular))
-            }
+    let u = d3.select('#content g.map')
+              .selectAll("path")
+              .data(geoJson.features)
+              .join('path')
+              .attr('d', geoGenerator);
 
     })
-
  
 </script>
 
 
 <div id="content">
     <svg width="800px" height="400px">
-      <g id="map"></g>
+      <g class="map"></g>
     </svg>
   </div>
